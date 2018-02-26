4864d065466cbe161ab896a052d00ebc6e9083fe
wangtao <wangtao3343@sina.com>
2018-02-26 14:03:02 +0800

    删除无用代码

--- src/main/java/com/bayax/BayaxMain.java
+++ src/main/java/com/bayax/BayaxMain.java
@@ -2,13 +2,19 @@ package com.bayax;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BayaxMain {
+public class BayaxMain extends SpringBootServletInitializer {
 
     public static void main(String args[]){
         SpringApplication.run(BayaxMain.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BayaxMain.class);
+    }
 
 }
